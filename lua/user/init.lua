87b7a075a8d845f7ca39605ced28d9cdb37caa91
Krishnakumar Gopalakrishnan <krishna.kumar@ucl.ac.uk>
2022-05-03 17:51:18 +0100

    fixes typo wherein BufferLineCycle Next/Prev were swapped

--- lua/user/init.lua
+++ lua/user/init.lua
@@ -826,8 +826,8 @@ local config = {
 
     " These commands will navigate through buffers in order regardless of which mode you are using
     " e.g. if you change the order of buffers :bnext and :bprevious will not respect the custom ordering
-    nnoremap <silent>[b :BufferLineCycleNext<CR>
-    nnoremap <silent>]b :BufferLineCyclePrev<CR>
+    nnoremap <silent>[b :BufferLineCyclePrev<CR>
+    nnoremap <silent>]b :BufferLineCycleNext<CR>
     " nnoremap <silent><leader>Bd :BufferLineSortByDirectory<CR>
     " nnoremap <silent><leader>Be :BufferLineSortByExtension<CR>
     " nnoremap <silent><leader>Bp :BufferLinePick<CR>
