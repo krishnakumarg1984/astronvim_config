96d85b919c5f23582f0f47bb32f38c22f30701bf
Krishnakumar Gopalakrishnan <krishna.kumar@ucl.ac.uk>
2022-05-04 22:44:56 +0100

    finished configuring bufferline

--- lua/user/init.lua
+++ lua/user/init.lua
@@ -305,6 +305,50 @@ local config = {
     end,
     bufferline = {
       options = {
+        -- numbers = "ordinal",
+        close_command = "Bdelete %d", -- can be a string | function, see "Mouse actions"
+        diagnostics = "nvim_lsp",
+        diagnostics_indicator = function(count, level, diagnostics_dict, context)
+          if context.buffer:current() then
+            return ''
+          end
+          local icon = level:match("error") and " " or " "
+          return " " .. icon .. count
+          -- local s = " "
+          -- for e, n in pairs(diagnostics_dict) do
+          --   local sym = e == "error" and " "
+          --   or (e == "warning" and " " or "" )
+          --   s = s .. n .. sym
+          -- end
+          -- return s
+        end,
+        groups = {
+          options = {
+            toggle_hidden_on_enter = true -- when you re-enter a hidden group this options re-opens that group so the buffer is visible
+          },
+          items = {
+            {
+              name = "Tests", -- Mandatory
+              highlight = {gui = "underline", guisp = "blue"}, -- Optional
+              priority = 2, -- determines where it will appear relative to other groups (Optional)
+              icon = "", -- Optional
+              matcher = function(buf) -- Mandatory
+                return buf.name:match('%_test') or buf.name:match('%_spec')
+              end,
+            },
+            {
+              name = "Docs",
+              highlight = {gui = "undercurl", guisp = "green"},
+              auto_close = false,  -- whether or not close this group if it doesn't contain the current buffer
+              matcher = function(buf)
+                return buf.name:match('%.md') or buf.name:match('%.txt')
+              end,
+              separator = { -- Optional
+                style = require('bufferline.groups').separator.tab
+              },
+            }
+          }
+        },
         offsets = {
           {
             filetype = "neo-tree",
@@ -312,6 +356,33 @@ local config = {
             highlight = "Directory",
             text_align = "left"
           }
+        },
+        custom_areas = {
+          right = function()
+            local result = {}
+            local seve = vim.diagnostic.severity
+            local error = #vim.diagnostic.get(0, {severity = seve.ERROR})
+            local warning = #vim.diagnostic.get(0, {severity = seve.WARN})
+            local info = #vim.diagnostic.get(0, {severity = seve.INFO})
+            local hint = #vim.diagnostic.get(0, {severity = seve.HINT})
+
+            if error ~= 0 then
+              table.insert(result, {text = "  " .. error, guifg = "#EC5241"})
+            end
+
+            if warning ~= 0 then
+              table.insert(result, {text = "  " .. warning, guifg = "#EFB839"})
+            end
+
+            if hint ~= 0 then
+              table.insert(result, {text = "  " .. hint, guifg = "#A3BA5E"})
+            end
+
+            if info ~= 0 then
+              table.insert(result, {text = "  " .. info, guifg = "#7EA9A7"})
+            end
+            return result
+          end,
         },
       },
     },
@@ -460,9 +531,12 @@ local config = {
       ensure_installed = { "sumneko_lua" },
     },
     -- which_key = {
-    --   presets = {
-    --     operators = true, -- adds help for operators like d, y, ... and registers them for motion / text object completion
-    --   },
+    --   operators = { gc = "Comments" },
+    --   -- plugins = {
+    --   --   presets = {
+    --   --     operators = true, -- adds help for operators like d, y, ... and registers them for motion / text object completion
+    --   --   },
+    --   -- },
     -- },
     packer = {
       compile_path = vim.fn.stdpath "config" .. "/lua/packer_compiled.lua",
@@ -973,10 +1047,13 @@ local config = {
     " e.g. if you change the order of buffers :bnext and :bprevious will not respect the custom ordering
     nnoremap <silent>[b :BufferLineCyclePrev<CR>
     nnoremap <silent>]b :BufferLineCycleNext<CR>
-    " nnoremap <silent><leader>Bd :BufferLineSortByDirectory<CR>
-    " nnoremap <silent><leader>Be :BufferLineSortByExtension<CR>
-    " nnoremap <silent><leader>Bp :BufferLinePick<CR>
-    " nnoremap <silent><leader>Bc :BufferLinePickClose<CR>
+    nnoremap <silent><leader><leader>[ :BufferLineMovePrev<CR>
+    nnoremap <silent><leader><leader>] :BufferLineMoveNext<CR>
+    nnoremap <silent><leader><leader>d :BufferLineSortByDirectory<CR>
+    nnoremap <silent><leader><leader>e :BufferLineSortByExtension<CR>
+    nnoremap <silent><leader><leader>p :BufferLinePick<CR>
+    nnoremap <silent><leader><leader>c :BufferLinePickClose<CR>
+    nnoremap <silent><leader><leader>P :BufferLineTogglePin<CR>
     nnoremap <silent><leader>1 <Cmd>BufferLineGoToBuffer 1<CR>
     nnoremap <silent><leader>2 <Cmd>BufferLineGoToBuffer 2<CR>
     nnoremap <silent><leader>3 <Cmd>BufferLineGoToBuffer 3<CR>
