023ac797a7f8cc5af64c2cbfed5195bb77666044
Krishnakumar Gopalakrishnan <krishna.kumar@ucl.ac.uk>
2022-05-04 21:51:02 +0100

    adds bufferline offset for neo-tree

--- lua/user/init.lua
+++ lua/user/init.lua
@@ -303,18 +303,18 @@ local config = {
 
       return config
     end,
-    -- bufferline = {
-    --   diagnostics = "nvim_lsp",
-    --   -- count is an integer representing total count of errors
-    --   -- level is a string "error" | "warning"
-    --   -- diagnostics_dict is a dictionary from error level ("error", "warning" or "info")to number of errors for each level.
-    --   -- this should return a string
-    --   -- Don't get too fancy as this function will be executed a lot
-    --   diagnostics_indicator = function(count, level, diagnostics_dict, context)
-    --     local icon = level:match("error") and " " or " "
-    --     return " " .. icon .. count
-    --   end
-    -- },
+    bufferline = {
+      options = {
+        offsets = {
+          {
+            filetype = "neo-tree",
+            text = "File Explorer",
+            highlight = "Directory",
+            text_align = "left"
+          }
+        },
+      },
+    },
     gitsigns = {
       signs = {
         add = { hl = "GitSignsAdd", text = "▎", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
