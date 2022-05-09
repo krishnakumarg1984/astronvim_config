2e3ae05e1ac484405828a9cad7801d1cf6fa4cb4
Krishnakumar Gopalakrishnan <krishna.kumar@ucl.ac.uk>
2022-05-09 11:39:20 +0100

    updates to lsp-installer

--- lua/user/lsp-installer.lua
+++ lua/user/lsp-installer.lua
@@ -1,30 +1,11 @@
 -- vim: foldmarker=(((,))):foldmethod=marker:foldlevel=0:shiftwidth=2:softtabstop=2:tabstop=2
 
 local status_ok, lsp_installer = pcall(require, "nvim-lsp-installer")
-if not status_ok then
-  return
-end
-
--- Provide settings first!
-lsp_installer.settings({
-  ui = {
-    icons = {
-      server_installed = "✓",
-      server_pending = "➜",
-      server_uninstalled = "✗",
-    },
-  },
-})
 
 -- Register a handler that will be called for all installed servers.
 -- Alternatively, you may also register handlers on specific server instances instead (see example below).
 -- local function make_server_ready()
 lsp_installer.on_server_ready(function(server)
-  local opts = {
-    on_attach = require("user.lsp.handlers").on_attach,
-    capabilities = require("user.lsp.handlers").capabilities,
-  }
-
   if server.name == "clangd" then
     -- NOTE: Workaround for "warning: multiple different client offset_encodings detected for buffer, this is not supported yet".
     -- Ref: https://github.com/jose-elias-alvarez/null-ls.nvim/issues/428#issuecomment-997226723
@@ -33,41 +14,11 @@ lsp_installer.on_server_ready(function(server)
     opts.capabilities.memoryUsageProvider = true
   end
 
-  if server.name == "jsonls" then
-    local jsonls_opts = require("user.lsp.settings.jsonls")
-    opts = vim.tbl_deep_extend("force", jsonls_opts, opts)
-  end
-
-  if server.name == "sumneko_lua" then
-    local sumneko_opts = require("user.lsp.settings.sumneko_lua")
-    opts = vim.tbl_deep_extend("force", sumneko_opts, opts)
-  end
-
-  -- if server.name == "pyright" then
-  --   local pyright_opts = require("user.lsp.settings.pyright")
-  --   opts = vim.tbl_deep_extend("force", pyright_opts, opts)
-  -- end
-
   -- This setup() function is exactly the same as lspconfig's setup function.
   -- Refer to https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
   server:setup(opts)
   vim.cmd([[ do User LspAttachBuffers ]])
 end)
--- end
-
-local function install_server(server)
-  local lsp_installer_servers = require("nvim-lsp-installer.servers")
-  -- local ok, server_analyzer = lsp_installer_servers.get_server(server)
-  local server_available, requested_server = lsp_installer_servers.get_server(server)
-  if server_available then
-    if not requested_server:is_installed() then
-      -- Queue the server to be installed
-      requested_server:install()
-      -- server_analyzer:install(server) -- will install in background
-      -- lsp_installer.install(server) -- install window will popup
-    end
-  end
-end
 
 -- Enable some language servers with the additional completion capabilities offered by nvim-cmp
 local servers = {
