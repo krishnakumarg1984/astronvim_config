2c9bbab41032efc7e054fda1e0568f287fc87d8e
Krishnakumar Gopalakrishnan <krishna.kumar@ucl.ac.uk>
2022-05-09 01:07:05 +0100

    fixes a config error in bufferline

--- lua/user/init.lua
+++ lua/user/init.lua
@@ -378,33 +378,33 @@ local config = {
           -- end
           -- return s
         end,
-        groups = {
-          options = {
-            toggle_hidden_on_enter = true -- when you re-enter a hidden group this options re-opens that group so the buffer is visible
-          },
-          items = {
-            {
-              name = "Tests", -- Mandatory
-              highlight = {gui = "underline", guisp = "blue"}, -- Optional
-              priority = 2, -- determines where it will appear relative to other groups (Optional)
-              icon = "", -- Optional
-              matcher = function(buf) -- Mandatory
-                return buf.name:match('%_test') or buf.name:match('%_spec')
-              end,
-            },
-            {
-              name = "Docs",
-              highlight = {gui = "undercurl", guisp = "green"},
-              auto_close = false,  -- whether or not close this group if it doesn't contain the current buffer
-              matcher = function(buf)
-                return buf.name:match('%.md') or buf.name:match('%.txt')
-              end,
-              separator = { -- Optional
-                style = require('bufferline.groups').separator.tab
-              },
-            }
-          }
-        },
+        -- groups = {
+        --   options = {
+        --     toggle_hidden_on_enter = true -- when you re-enter a hidden group this options re-opens that group so the buffer is visible
+        --   },
+        --   items = {
+        --     {
+        --       name = "Tests", -- Mandatory
+        --       highlight = {gui = "underline", guisp = "blue"}, -- Optional
+        --       priority = 2, -- determines where it will appear relative to other groups (Optional)
+        --       icon = "", -- Optional
+        --       matcher = function(buf) -- Mandatory
+        --         return buf.name:match('%_test') or buf.name:match('%_spec')
+        --       end,
+        --     },
+        --     {
+        --       name = "Docs",
+        --       highlight = {gui = "undercurl", guisp = "green"},
+        --       auto_close = false,  -- whether or not close this group if it doesn't contain the current buffer
+        --       matcher = function(buf)
+        --         return buf.name:match('%.md') or buf.name:match('%.txt')
+        --       end,
+        --       separator = { -- Optional
+        --         style = require('bufferline.groups').separator.tab
+        --       },
+        --     }
+        --   }
+        -- },
         offsets = {
           {
             filetype = "neo-tree",
@@ -1702,8 +1702,9 @@ local config = {
 
     -- )))
 
-    -- )))
   end,
+
+-- )))
 }
 
 -- )))
