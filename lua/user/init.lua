25cc855c7bacd238b0194fb0bb2a499d33406c5a
Krishnakumar Gopalakrishnan <krishna.kumar@ucl.ac.uk>
2022-05-17 18:34:28 +0100

    install tabout.nvim

--- lua/user/init.lua
+++ lua/user/init.lua
@@ -601,6 +601,31 @@ local config = {
           require("telescope").load_extension "projects"
         end,
       },
+      ["abecodes/tabout.nvim"] = {
+        after = "nvim-cmp",
+        config = function()
+          require("tabout").setup {
+            tabkey = "<Tab>", -- key to trigger tabout, set to an empty string to disable
+            backwards_tabkey = "<S-Tab>", -- key to trigger backwards tabout, set to an empty string to disable
+            act_as_tab = true, -- shift content if tab out is not possible
+            act_as_shift_tab = false, -- reverse shift content if tab out is not possible (if your keyboard/terminal supports <S-Tab>)
+            default_tab = "<C-t>", -- shift default action (only at the beginning of a line, otherwise <TAB> is used)
+            default_shift_tab = "<C-d>", -- reverse shift default action,
+            enable_backwards = true, -- well ...
+            completion = true, -- if the tabkey is used in a completion pum
+            tabouts = {
+              { open = "'", close = "'" },
+              { open = '"', close = '"' },
+              { open = "`", close = "`" },
+              { open = "(", close = ")" },
+              { open = "[", close = "]" },
+              { open = "{", close = "}" },
+            },
+            ignore_beginning = true, --[[ if the cursor is at the beginning of a filled element it will rather tab out than shift the content ]]
+            exclude = {}, -- tabout will ignore these filetypes
+          }
+        end,
+      },
       {
         "folke/trouble.nvim",
         -- ft = { "c", "cpp", "cuda", "fortran", "lua", "rust" }
@@ -2211,9 +2236,6 @@ return config
 -- Evaluate these plugins next (((
 
 -- https://github.com/mfussenegger/nvim-dap-python
--- https://github.com/gbprod/substitute.nvim
--- use { "ur4ltz/surround.nvim" }
--- https://github.com/abecodes/tabout.nvim
 -- https://github.com/jamestthompson3/nvim-remote-containers  -- Develop inside docker containers, just like VSCode
 -- https://github.com/anufrievroman/vim-angry-reviewer
 -- use { "Konfekt/vim-sentence-chopper" }
@@ -2278,6 +2300,8 @@ return config
 -- https://github.com/rlane/pounce.nvim
 -- https://github.com/filipdutescu/renamer.nvim
 -- https://github.com/rmagatti/session-lens
+-- https://github.com/gbprod/substitute.nvim
+-- use { "ur4ltz/surround.nvim" }
 -- use { "nvim-telescope/telescope-media-files.nvim" }
 -- use { "nvim-telescope/telescope-packer.nvim" }
 -- use { "cljoly/telescope-repo.nvim" }
