b7c7c13a2aa86d077ed6b76a461049812dcdf03f
Krishnakumar Gopalakrishnan <krishna.kumar@ucl.ac.uk>
2022-05-17 18:13:12 +0100

    Adds event_handler for neo-tree

--- lua/user/init.lua
+++ lua/user/init.lua
@@ -924,6 +924,19 @@ local config = {
     ["neo-tree"] = {
       close_if_last_window = true, -- Close Neo-tree if it is the last window left in the tab
       enable_diagnostics = true,
+      event_handlers = {
+        {
+          event = "after_render",
+          handler = function(_)
+            if vim.bo.filetype == "neo-tree" then
+              local status_ok, alpha = pcall(require, "alpha")
+              if status_ok then
+                alpha.redraw()
+              end
+            end
+          end,
+        },
+      },
     },
 
     -- )))
