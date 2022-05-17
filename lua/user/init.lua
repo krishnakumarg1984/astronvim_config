a7d3b43c9d082c2a38408596ecab190767ef82ec
Krishnakumar Gopalakrishnan <krishna.kumar@ucl.ac.uk>
2022-05-17 17:31:51 +0100

    default_scheme telescope highlights

--- lua/user/init.lua
+++ lua/user/init.lua
@@ -198,9 +198,30 @@ local config = {
   default_theme = {
     diagnostics_style = { italic = true },
     -- Modify the color table
-    colors = {
-      fg = "#abb2bf",
-    },
+    colors = function(C)
+      C.telescope_green = C.green
+      C.telescope_red = C.red
+      C.telescope_fg = C.fg
+      C.telescope_bg = C.black_1
+      C.telescope_bg_alt = C.bg_1
+      return C
+    end,
+    highlights = function(hl)
+      local C = require "default_theme.colors"
+      hl.TelescopeBorder = { fg = C.telescope_bg_alt, bg = C.telescope_bg }
+      hl.TelescopeNormal = { bg = C.telescope_bg }
+      hl.TelescopePreviewBorder = { fg = C.telescope_bg, bg = C.telescope_bg }
+      hl.TelescopePreviewNormal = { bg = C.telescope_bg }
+      hl.TelescopePreviewTitle = { fg = C.telescope_bg, bg = C.telescope_green }
+      hl.TelescopePromptBorder = { fg = C.telescope_bg_alt, bg = C.telescope_bg_alt }
+      hl.TelescopePromptNormal = { fg = C.telescope_fg, bg = C.telescope_bg_alt }
+      hl.TelescopePromptPrefix = { fg = C.telescope_red, bg = C.telescope_bg_alt }
+      hl.TelescopePromptTitle = { fg = C.telescope_bg, bg = C.telescope_red }
+      hl.TelescopeResultsBorder = { fg = C.telescope_bg, bg = C.telescope_bg }
+      hl.TelescopeResultsNormal = { bg = C.telescope_bg }
+      hl.TelescopeResultsTitle = { fg = C.telescope_bg, bg = C.telescope_bg }
+      return hl
+    end,
   },
 
   --- )))
