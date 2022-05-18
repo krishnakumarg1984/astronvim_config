8eedac91f6acbe2e2fd6ee8d9f9ec25d63810195
Krishnakumar Gopalakrishnan <krishna.kumar@ucl.ac.uk>
2022-05-18 19:54:17 +0100

    Couple of commented out indent-blankline settings

--- lua/user/init.lua
+++ lua/user/init.lua
@@ -956,6 +956,8 @@ local config = {
 
     indent_blankline = {
       show_first_indent_level = false,
+      -- show_current_context_start = true,
+      -- space_char_blankline = " ",
     },
 
     -- )))
