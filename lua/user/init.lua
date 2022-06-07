8e733655f62be3af748880d49085a0cdccd8fc2e
Krishnakumar Gopalakrishnan <krishna.kumar@ucl.ac.uk>
2022-06-07 15:01:16 +0100

    uses newly added custom position provider in feline to setup row/column padding

--- lua/user/init.lua
+++ lua/user/init.lua
@@ -1207,7 +1207,8 @@ local config = {
     -- 'feline' override setup() (((
 
     feline = function(config)
-      config.components.active[2][6] = { provider = { name = "position", opts = { padding = true } } }
+      -- config.components.active[2][6] = { provider = { name = "position", opts = { padding = true } } }
+      config.components.active[2][6] = { provider = { name = "position", opts = { padding = { line = 4, col = 2 } } } }
       -- vim.pretty_print(config.components.active[2])
       return config
     end,
