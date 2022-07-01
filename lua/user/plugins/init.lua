a2af2dedfd06aed63fbabcb9c2966a3dd56cd392
Krishnakumar Gopalakrishnan <krishna.kumar@ucl.ac.uk>
2022-07-01 15:00:12 +0100

    comments out vimspector

--- lua/user/plugins/init.lua
+++ lua/user/plugins/init.lua
@@ -149,6 +149,7 @@ return {
     -- requires = { "machakann/vim-highlightedyank" },
   },
   { "tpope/vim-unimpaired", keys = { "[", "]", "=", "<", ">" } }, -- e (exchange), ob (background), oc (cursorline), od (diff), oh (hlsearch), oi (ignorecase), ol (list), on (number), or (relativenumber), os (spell), ot (colorcolumn), ou (cursorcolumn), ov (virtualedit), ow (wrap), ox (cursorline cursorcolumn), u (encode/decode urls), y (C-string style escaping), n (conflict marker), a (args), b (buffers), l (location), q (quickfix), t (tag), >p Paste after linewise, increasing indent, >P (Paste before linewise, increasing indent), <p (Paste after linewise, decreasing indent), <P (Paste before linewise, decreasing indent), =p (Paste after linewise, reindenting), =P (Paste before linewise, reindenting), x<motion> (xml encode/decode)
+  -- { "puremourning/vimspector", cmd = { "VimspectorInstall", "VimspectorUpdate" } },
   { "lervag/vimtex" }, -- ft = "tex" },
   -- { "axvr/zepl.vim" },
   -- { "svban/YankAssassin.vim" },
