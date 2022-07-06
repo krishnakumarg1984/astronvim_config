23768585c2289435cc911d3940b725f3f9e7aed7
Krishnakumar Gopalakrishnan <krishna.kumar@ucl.ac.uk>
2022-07-06 14:43:13 +0100

    minor comment update to packer config

--- lua/user/plugins/packer.lua
+++ lua/user/plugins/packer.lua
@@ -1,5 +1,6 @@
 return function(config)
   config.display = nil -- no floating window for packer
   -- config.compile_path = vim.fn.stdpath "config" .. "/lua/packer_compiled.lua"
+  -- compile_path = vim.fn.stdpath "data" .. "/packer_compiled.lua",
   return config
 end
