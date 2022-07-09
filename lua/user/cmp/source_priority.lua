e8974490e60c4592e27b2f0d9cf3301fc0ad9992
Krishnakumar Gopalakrishnan <krishna.kumar@ucl.ac.uk>
2022-07-09 17:22:26 +0100

    cmp config split into 2 files and moved as per discord chat with mehalter into lua/user/cmp/ folder

---
+++ lua/user/cmp/source_priority.lua
@@ -0,0 +1,16 @@
+-- modify here the priorities of default cmp sources
+-- higher value == higher priority
+-- The value can also be set to a boolean for disabling default sources:
+-- false == disabled
+-- true == 1000
+return {
+  luasnip = 1000,
+  git = 900,
+  nvim_lsp = 750,
+  nvim_lsp_signature_help = 700,
+  pandoc_references = 600,
+  buffer = 500,
+  path = 250,
+  emoji = 200,
+  dictionary = 150,
+}
