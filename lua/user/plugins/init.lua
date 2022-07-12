e9e4364679de22f849f9c1295313ea95f61c1bc6
Krishnakumar Gopalakrishnan <krishna.kumar@ucl.ac.uk>
2022-07-12 14:50:01 +0100

    minor commentary additions for plugin description in init.lua

--- lua/user/plugins/init.lua
+++ lua/user/plugins/init.lua
@@ -5,7 +5,7 @@ return {
 
   ["max397574/better-escape.nvim"] = { disable = true }, -- Escape from insert mode without delay when typing
   ["rcarriga/nvim-notify"] = { disable = true }, -- A fancy, configurable, notification manager for NeoVim
-  ["p00f/nvim-ts-rainbow"] = { disable = true }, -- Rainbow parentheses for neovim using tree-sitter. https://sr.ht/~p00f/nvim-ts-rainbow preferred
+  ["p00f/nvim-ts-rainbow"] = { disable = true }, -- Rainbow parentheses for neovim using tree-sitter
 
   -- )))
 
@@ -13,22 +13,22 @@ return {
 
   -- Additional vimscript plugins (((
 
-  -- ["tweekmonster/startuptime.vim"] = { cmd = { "StartupTime" } }, -- Breakdown Vim's --startuptime output
-  -- ["dstein64/vim-startuptime"] = { cmd = { "StartupTime" } }, -- A Vim plugin for profiling Vim's startup time.
+  -- ["tweekmonster/startuptime.vim"] = { cmd = { "StartupTime" } }, -- Breakdown Vim's '--startuptime' output
+  -- ["dstein64/vim-startuptime"] = { cmd = { "StartupTime" } }, -- A Vim plugin for profiling Vim's startup time
   ["tyru/capture.vim"] = { cmd = { "Capture" } }, -- Show Ex command output in buffer
-  ["lambdalisue/suda.vim"] = { cmd = { "SudaRead", "SudaWrite" } }, -- An alternative sudo.vim for Vim and Neovim, limited support sudo in Windows
+  ["lambdalisue/suda.vim"] = { cmd = { "SudaRead", "SudaWrite" } }, -- An alternative sudo.vim for Vim and Neovim
   ["mg979/tasks.vim"] = require "user.plugins.tasks_mg979", -- Async jobs and tasks
-  -- ["kenn7/vim-arsync"] = require "user.plugins.vim-arsync",
-  ["gauteh/vim-cppman"] = { cmd = { "Cppman" } },
-  ["Konfekt/vim-DetectSpellLang"] = require "user.plugins.vim-detectspelllang",
-  ["dbmrq/vim-dialect"] = require "user.plugins.vim-dialect",
-  ["Konfekt/vim-sentence-chopper"] = { ft = { "tex", "markdown", "text" } },
-  ["andymass/vim-matchup"] = { after = "nvim-treesitter" },
-  ["urbainvaes/vim-ripple"] = require "user.plugins.vim-ripple",
-  ["tpope/vim-unimpaired"] = require "user.plugins.vim-unimpaired",
-  ["lervag/vimtex"] = {},
-  -- ["svban/YankAssassin.vim"] = {},
-  -- ["axvr/zepl.vim"] = {},
+  -- ["kenn7/vim-arsync"] = require "user.plugins.vim-arsync", -- Synchronisation of remote & local files using rsync
+  ["gauteh/vim-cppman"] = { cmd = { "Cppman" } }, -- cppman ("C++ 98/11/14 manual pages for Linux/MacOS" ) from vim
+  ["Konfekt/vim-DetectSpellLang"] = require "user.plugins.vim-detectspelllang", -- Autodetect the spellcheck language
+  ["dbmrq/vim-dialect"] = require "user.plugins.vim-dialect", -- Project specific spellfiles
+  ["Konfekt/vim-sentence-chopper"] = { ft = { "tex", "markdown", "text" } }, -- VCS-friendly text formatting
+  ["andymass/vim-matchup"] = { after = "nvim-treesitter" }, -- Even better % navigate & highlight matching words
+  ["urbainvaes/vim-ripple"] = require "user.plugins.vim-ripple", -- Minimalist plugin to interact with a REPL
+  ["tpope/vim-unimpaired"] = require "user.plugins.vim-unimpaired", -- Pairs of handy bracket mappings
+  ["lervag/vimtex"] = {}, --  VimTeX: A modern Vim and neovim filetype plugin for LaTeX files
+  -- ["svban/YankAssassin.vim"] = {}, -- Don't let the cursor move while Yanking in Vim/Neovim
+  -- ["axvr/zepl.vim"] = {}, -- Simple and minimal REPL integration plugin for Vim and Neovim
 
   -- ))) end of additional vimscript plugins
 
@@ -42,15 +42,15 @@ return {
   -- ["octaltree/cmp-look"] = require "user.plugins.cmp-look",
   ["gpanders/editorconfig.nvim"] = {}, --  EditorConfig plugin for Neovim
   ["akinsho/git-conflict.nvim"] = require "user.plugins.git-conflict", -- Visualise & resolve merge conflicts
-  ["ellisonleao/glow.nvim"] = { cmd = { "Glow", "GlowInstall" } }, -- A markdown preview directly in your neovim.
-  -- ["JASONews/glow-hover"] = { after = "nvim-lsp-installer" }, -- A Neovim plugin leveraging glow for LSPs hover responses.
-  -- ["lukas-reineke/headlines.nvim"] = require "user.plugins.headlines", -- horizontal highlights for text filetypes
+  ["ellisonleao/glow.nvim"] = { cmd = { "Glow", "GlowInstall" } }, -- A markdown preview directly in your neovim
+  -- ["JASONews/glow-hover"] = { after = "nvim-lsp-installer" }, -- Leverages 'glow' for LSPs hover responses
+  -- ["lukas-reineke/headlines.nvim"] = require "user.plugins.headlines", -- Horizontal highlights for text filetypes
   -- ["hkupty/iron.nvim"] = require "user.plugins.iron", -- Interactive Repl Over Neovim
-  ["rebelot/kanagawa.nvim"] = {}, -- Colorscheme inspired by the colors of the famous painting by Katsushika Hokusai.
+  ["rebelot/kanagawa.nvim"] = {}, -- Colorscheme inspired by the colors of the famous painting by Katsushika Hokusai
   ["ray-x/lsp_signature.nvim"] = require "user.plugins.lsp_signature", -- LSP signature hint as you type
-  -- ["chentoast/marks.nvim"] = require "user.plugins.marks", -- A better UX for viewing & interacting with Vim marks.
+  -- ["chentoast/marks.nvim"] = require "user.plugins.marks", -- A better UX for viewing & interacting with Vim marks
   ["echasnovski/mini.nvim"] = require "user.plugins.mini", -- Collection of minimal, independent, & fast Lua modules
-  ["jghauser/mkdir.nvim"] = { event = "BufWritePre" }, -- This neovim plugin creates missing folders on save.
+  ["jghauser/mkdir.nvim"] = { event = "BufWritePre" }, -- This neovim plugin creates missing folders on save
   ["jbyuki/nabla.nvim"] = { module = "nabla" }, -- Take your scientific notes in Neovim
   ["danymat/neogen"] = require "user.plugins.neogen", -- Annotation generator (multiple languages & conventions)
   ["TimUntersberger/neogit"] = require "user.plugins.neogit", -- Magit for neovim
@@ -70,7 +70,7 @@ return {
   -- { "eddiebergman/nvim-treesitter-pyfold", after = { "nvim-treesitter" }, ft = "python" }, -- Sane Python folding
   ["nvim-treesitter/nvim-treesitter-refactor"] = { after = "nvim-treesitter" }, -- Treesitter refactor module
   ["nvim-treesitter/nvim-treesitter-textobjects"] = { after = "nvim-treesitter" },
-  ["RRethy/nvim-treesitter-textsubjects"] = require "user.plugins.nvim-treesitter-textsubjects", -- dwim textobjects
+  ["RRethy/nvim-treesitter-textsubjects"] = require "user.plugins.nvim-treesitter-textsubjects", -- DWIM textobjects
   -- ["s1n7ax/nvim-window-picker"] = {}, -- Prompts to pick a window & returns its window id
   ["samjwill/nvim-unception"] = {}, -- Open files from within Neovim's terminal emulator without weird behavior
   ["ahmedkhalf/project.nvim"] = require "user.plugins.project", -- The superior project management solution for nvim
@@ -78,11 +78,11 @@ return {
   ["linty-org/readline.nvim"] = require "user.plugins.readline", -- Readline motions and deletions in Neovim
   ["michaelb/sniprun"] = require "user.plugins.sniprun", -- Run lines/blocs of code, supports multiples languages
   -- ["lewis6991/spellsitter.nvim"] = require "user.plugins.spellsitter", -- Treesitter powered spellchecker
-  -- ["luukvbaal/stabilize.nvim"] = require "user.plugins.stabilize", -- Stabilize window open/close events.
+  -- ["luukvbaal/stabilize.nvim"] = require "user.plugins.stabilize", -- Stabilize window open/close events
   -- ["abecodes/tabout.nvim"] = require "user.plugins.tabout", -- tabout plugin for neovim
   -- ["benfowler/telescope-luasnip.nvim"] = require "user.plugins.telescope-luasnip", -- Telescope+LuaSnip integration
   ["johmsalas/text-case.nvim"] = require "user.plugins.text-case", -- An all in one plugin for converting text case
-  ["folke/trouble.nvim"] = require "user.plugins.trouble", -- Helps you solve all the trouble your code is causing.
+  ["folke/trouble.nvim"] = require "user.plugins.trouble", -- Helps you solve all the trouble your code is causing
 
   -- ))) -- end of additional lua plugins
 
@@ -90,9 +90,9 @@ return {
 
   -- Modify AstroNvim default plugins (((
 
-  ["jose-elias-alvarez/null-ls.nvim"] = require "user.plugins.null-ls",
-  ["norcalli/nvim-colorizer.lua"] = { event = nil, ft = { "html", "javascript", "css" } }, -- disable more filetypes
-  ["windwp/nvim-ts-autotag"] = require "user.plugins.nvim-ts-autotag", -- treesitter to auto close & rename html tags
+  ["jose-elias-alvarez/null-ls.nvim"] = require "user.plugins.null-ls", -- Inject LSP diagnostics, code actions etc
+  ["norcalli/nvim-colorizer.lua"] = { event = nil, ft = { "html", "javascript", "css" } }, -- Neovim colorizer
+  ["windwp/nvim-ts-autotag"] = require "user.plugins.nvim-ts-autotag", -- Autoclose & rename html tags with treesitter
 
   -- ))) end of modification of default AstroNvim plugins
 }
