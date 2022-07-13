d160a647959da667658fa21607c3de54e3992856
Krishnakumar Gopalakrishnan <krishna.kumar@ucl.ac.uk>
2022-07-13 19:58:25 +0100

    sets up file ignore patterns for telescope

--- lua/user/plugins/telescope.lua
+++ lua/user/plugins/telescope.lua
@@ -1,4 +1,21 @@
 return {
+  -- https://gitlab.com/HiPhish/nvim-config/-/blob/master/plugin/telescope.lua
+  defaults = {
+    -- winblend = 30,
+    -- border = true,
+    file_ignore_patterns = {
+      "%.zip$",
+      "%.tar$",
+      "%.tar.gz$",
+      "%.so$",
+      "%.a$",
+      "%.fasl$",
+      "%.pyc$",
+      "%.whl$",
+      "%.bin$",
+      "%.db$",
+    },
+  },
   pickers = {
     diagnostics = { theme = "dropdown" },
     -- diagnostics = { layout = { "vertical" } },
