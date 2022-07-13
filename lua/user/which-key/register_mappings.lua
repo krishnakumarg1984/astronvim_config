eaee8c38e5e08d0bbe5ac94273a36fa6517b2d21
Krishnakumar Gopalakrishnan <krishna.kumar@ucl.ac.uk>
2022-07-13 19:26:05 +0100

    fixes which-key dap mappings

--- lua/user/which-key/register_mappings.lua
+++ lua/user/which-key/register_mappings.lua
@@ -44,24 +44,33 @@ return {
       D = { "<cmd>windo diffthis<CR>", "diff buffers" },
       d = {
         name = "Debug",
-        b = {
-          function() require("dap").set_breakpoint(vim.fn.input "Breakpoint condition: ") end,
-          "Set conditional breakpoint",
-        },
         B = {
           function() require("dap").list_breakpoints() end,
           "List breakpoints",
         },
+        b = { "<cmd>lua require'dap'.step_back()<cr>", "Step Back" },
+        -- C = { "<cmd>lua require'dap'.set_breakpoint(vim.fn.input '[Condition] > ')<cr>", "Conditional Breakpoint" },
         C = {
+          function() require("dap").set_breakpoint(vim.fn.input "Breakpoint condition: ") end,
+          "Set conditional breakpoint",
+        },
+        c = {
           function() require("dap").clear_breakpoints() end,
           "Clear breakpoints",
         },
+        -- c = { "<cmd>lua require'dap'.continue()<cr>", "Continue" },
         -- c = {
         --   function()
         --     require("dap").continue()
         --   end,
         --   "Continue",
         -- },
+        d = { "<cmd>lua require'dap'.disconnect()<cr>", "Disconnect" },
+        E = { "<cmd>lua require'dapui'.eval(vim.fn.input '[Expression] > ')<cr>", "Evaluate Input" },
+        e = { "<cmd>lua require'dapui'.eval()<cr>", "Evaluate" },
+        g = { "<cmd>lua require'dap'.session()<cr>", "Get Session" },
+        h = { "<cmd>lua require'dap.ui.widgets'.hover()<cr>", "Hover Variables" },
+        -- i = { "<cmd>lua require'dap'.step_into()<cr>", "Step Into" },
         -- i = {
         --   function()
         --     require("dap").step_into()
@@ -76,28 +85,33 @@ return {
           function() require("dap").run_last() end,
           "Run last debugged",
         },
+        -- o = { "<cmd>lua require'dap'.step_over()<cr>", "Step Over" },
         -- o = {
         --   function()
         --     require("dap").step_over()
         --   end,
         --   "Step Over",
         -- },
-        q = {
-          function() require("dap").close() end,
-          "Close debug session",
-        },
+        p = { "<cmd>lua require'dap'.pause.toggle()<cr>", "Pause" },
+        q = { "<cmd>lua require'dap'.close()<cr>", "Quit" },
+        -- q = {
+        --   function() require("dap").close() end,
+        --   "Close debug session",
+        -- },
         Q = {
           function() require("dap").terminate() end,
           "Terminate debug session",
         },
-        r = {
-          function() require("dap").repl.toggle() end,
-          "Debug REPL Toggle",
-        },
+        r = { "<cmd>lua require'dap'.repl.toggle()<cr>", "Toggle Repl" },
+        -- r = {
+        --   function() require("dap").repl.toggle() end,
+        --   "Debug REPL Toggle",
+        -- },
         R = {
           function() require("dap").repl.open() end,
           "Debug REPL Open",
         },
+        s = { "<cmd>lua require'dap.ui.widgets'.scopes()<cr>", "Scopes" },
         -- s = {
         --   function()
         --     require("dapui").float_element "scopes"
@@ -110,6 +124,7 @@ return {
         --   end,
         --   "Threads (debug-ui)",
         -- },
+        u = { "<cmd>lua require'dapui'.toggle()<cr>", "Toggle UI" },
         -- u = {
         --   function()
         --     require("dapui").toggle()
@@ -225,6 +240,14 @@ return {
       x = { name = "Diagnostics" },
     },
   },
+  v = {
+    ["leader"] = {
+      d = {
+        name = "Debug",
+        e = { "<cmd>lua require'dapui'.eval()<cr>", "Evaluate" },
+      },
+    },
+  },
 }
 
 -- https://github.com/David-Kunz/vim/blob/master/init.lua
@@ -235,3 +258,25 @@ return {
 -- vim.keymap.set('n', '<leader>hD', function() require"gitsigns".diffthis("~") end)
 -- vim.keymap.set('n', '<leader>hm', function() require"gitsigns".diffthis("main")end)
 -- vim.keymap.set('n', '<leader>hM', diffThisBranch)
+
+-- d = {
+--   name = "Debug",
+--   C = { "<cmd>lua require'dap'.set_breakpoint(vim.fn.input '[Condition] > ')<cr>", "Conditional Breakpoint" },
+--   U = { "<cmd>lua require'dapui'.toggle()<cr>", "Toggle UI" },
+--   b = { "<cmd>lua require'dap'.step_back()<cr>", "Step Back" },
+--   c = { "<cmd>lua require'dap'.continue()<cr>", "Continue" },
+--   d = { "<cmd>lua require'dap'.disconnect()<cr>", "Disconnect" },
+--   e = { "<cmd>lua require'dapui'.eval()<cr>", "Evaluate" },
+--   g = { "<cmd>lua require'dap'.session()<cr>", "Get Session" },
+--   h = { "<cmd>lua require'dap.ui.widgets'.hover()<cr>", "Hover Variables" },
+--   S = { "<cmd>lua require'dap.ui.widgets'.scopes()<cr>", "Scopes" },
+--   i = { "<cmd>lua require'dap'.step_into()<cr>", "Step Into" },
+--   o = { "<cmd>lua require'dap'.step_over()<cr>", "Step Over" },
+--   p = { "<cmd>lua require'dap'.pause.toggle()<cr>", "Pause" },
+--   q = { "<cmd>lua require'dap'.close()<cr>", "Quit" },
+--   r = { "<cmd>lua require'dap'.repl.toggle()<cr>", "Toggle Repl" },
+--   s = { "<cmd>lua require'dap'.continue()<cr>", "Start" },
+--   t = { "<cmd>lua require'dap'.toggle_breakpoint()<cr>", "Toggle Breakpoint" },
+--   x = { "<cmd>lua require'dap'.terminate()<cr>", "Terminate" },
+--   u = { "<cmd>lua require'dap'.step_out()<cr>", "Step Out" },
+-- },
