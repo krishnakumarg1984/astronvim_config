0b0120057e0d6bf0c5d90e0ff0397d1fbf077736
Krishnakumar Gopalakrishnan <krishna.kumar@ucl.ac.uk>
2022-07-18 20:09:03 +0100

    more clangd settings from renjithhedge

--- lua/user/lsp/server-settings/clangd.lua
+++ lua/user/lsp/server-settings/clangd.lua
@@ -3,4 +3,20 @@ return {
     offsetEncoding = "utf-8",
     memoryUsageProvider = true,
   },
+  filetypes = { "c", "cpp", "objc", "objcpp", "opencl" },
+  init_options = {
+    clangdFileStatus = true,
+  },
+  cmd = {
+    "clangd",
+    "--clang-tidy",
+    "--background-index",
+    "--all-scopes-completion",
+    "--header-insertion=iwyu",
+    "--completion-style=detailed",
+    "--suggest-missing-includes",
+    "--fallback-style=webkit",
+    "--cross-file-rename",
+    -- "--offset-encoding=utf-32",
+  },
 }
