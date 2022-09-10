ada6d643127f91563553a45ec9b6ff27062f2502
Krishnakumar Gopalakrishnan <krishna.kumar@ucl.ac.uk>
2022-09-10 13:08:27 +0100

    Comments out BufReadPost for the nvim-config-local plugin

--- lua/user/plugins/nvim-config-local/init.lua
+++ lua/user/plugins/nvim-config-local/init.lua
@@ -1,7 +1,7 @@
 return {
   -- fastest among all local-rc plugins
   -- after = "project.nvim",
-  event = "BufReadPost",
+  -- event = "BufReadPost",
   tag = "1.1.1",
   -- commit = "3f1e788",
   config = function() require("config-local").setup {} end,
