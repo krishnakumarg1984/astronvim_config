881f59edadef3781a548492d92f81f61879ef8e1
Krishnakumar Gopalakrishnan <krishna.kumar@ucl.ac.uk>
2022-09-27 18:20:25 +0100

    sets up padding=2 in the indent config for neo-tree

--- lua/user/plugins/neo-tree.lua
+++ lua/user/plugins/neo-tree.lua
@@ -1,1 +1,12 @@
-return { enable_diagnostics = true }
+return {
+  -- sort_case_insensitive = true, -- used when sorting files and directories in the tree
+  default_component_configs = {
+    -- container = {
+    --   enable_character_fade = false,
+    -- },
+    indent = {
+      padding = 2,
+    },
+  },
+  enable_diagnostics = true,
+}
