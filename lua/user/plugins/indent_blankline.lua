dc45b1c1d30b20396c4b3d0f749c0fc5bc3b8728
Krishnakumar Gopalakrishnan <krishna.kumar@ucl.ac.uk>
2022-09-27 18:17:40 +0100

    configures cpp as an excluded filetype for indent blankline

--- lua/user/plugins/indent_blankline.lua
+++ lua/user/plugins/indent_blankline.lua
@@ -3,4 +3,17 @@ return {
   show_end_of_line = true,
   -- show_current_context_start = true,
   -- space_char_blankline = " ",
+  filetype_exclude = {
+    "aerial",
+    "alpha",
+    "cpp",
+    "dashboard",
+    "help",
+    "neo-tree",
+    "neogitstatus",
+    "NvimTree",
+    "packer",
+    "startify",
+    "Trouble",
+  },
 }
