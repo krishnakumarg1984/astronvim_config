d5aebfabffac48a9aee77b9c28625e6d6288960a
Krishnakumar Gopalakrishnan <krishna.kumar@ucl.ac.uk>
2022-09-30 20:55:16 +0100

    updates to highlights.lua for 2.0

--- lua/user/highlights.lua
+++ lua/user/highlights.lua
@@ -1,5 +1,5 @@
---[[ -- Override highlight groups in any theme
-return {
+-- Override highlight groups in any theme
+highlights = {
   -- duskfox = { -- a table of overrides/changes to the default
   --   Normal = { bg = "#000000" },
   -- },
@@ -7,6 +7,13 @@ return {
     local C = require "default_theme.colors"
 
     highlights.Normal = { fg = C.fg, bg = C.bg }
+
+    -- New approach instead of diagnostic_style
+    highlights.DiagnosticError.italic = true
+    highlights.DiagnosticHint.italic = true
+    highlights.DiagnosticInfo.italic = true
+    highlights.DiagnosticWarn.italic = true
+
     return highlights
   end,
-} ]]
+}
