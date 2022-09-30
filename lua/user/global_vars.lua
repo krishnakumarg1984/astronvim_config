110dbcd611f5f1467e8d8ddfec86a8fee62a8d3e
Krishnakumar Gopalakrishnan <krishna.kumar@ucl.ac.uk>
2022-09-30 20:54:44 +0100

    moves the lsp server table from global vars on to lua/user/lsp/servers.lua

--- lua/user/global_vars.lua
+++ lua/user/global_vars.lua
@@ -83,23 +83,23 @@ M.kind_icons = {
   Variable = "",
 }
 
-M.lsp_servers = {
-  -- "arduino_language_server", -- written in 'go'
-  -- "asm_lsp", -- written in 'rust'
-  -- "ccls",
-  "clangd", -- requires a reasonably new version of glibc
-  "cmake",
-  -- "fortls", -- written in 'python'
-  -- "lemminx", -- XML Language Server written in 'java'
-  -- "ltex",
-  "marksman",
-  "prosemd_lsp",
-  -- "python-lsp-server",
-  -- "r_language_server", -- needs R in path
-  "sumneko_lua",
-  -- "pyright"
-  -- "taplo", -- problematic with rust/cargo installs
-  -- "zk", -- link not found (404 error)
-}
+-- M.lsp_servers = {
+--   -- "arduino_language_server", -- written in 'go'
+--   -- "asm_lsp", -- written in 'rust'
+--   -- "ccls",
+--   "clangd", -- requires a reasonably new version of glibc
+--   "cmake",
+--   -- "fortls", -- written in 'python'
+--   -- "lemminx", -- XML Language Server written in 'java'
+--   -- "ltex",
+--   "marksman",
+--   "prosemd_lsp",
+--   -- "python-lsp-server",
+--   -- "r_language_server", -- needs R in path
+--   "lua",
+--   -- "pyright"
+--   -- "taplo", -- problematic with rust/cargo installs
+--   -- "zk", -- link not found (404 error)
+-- }
 
 return M
--- lua/user/lsp/servers.lua
+++ lua/user/lsp/servers.lua
@@ -1,2 +1,19 @@
 -- Extend LSP configuration
-return require("user.global_vars").lsp_servers
+return {
+  -- "arduino_language_server", -- written in 'go'
+  -- "asm_lsp", -- written in 'rust'
+  -- "ccls",
+  -- "clangd", -- requires a reasonably new version of glibc
+  "cmake", --
+  -- "fortls", -- written in 'python'
+  -- "lemminx", -- XML Language Server written in 'java'
+  -- "ltex",
+  "marksman",
+  "prosemd_lsp",
+  -- "python-lsp-server",
+  -- "r_language_server", -- needs R in path
+  "sumneko_lua",
+  -- "pyright"
+  -- "taplo", -- problematic with rust/cargo installs
+  -- "zk", -- link not found (404 error)
+}
