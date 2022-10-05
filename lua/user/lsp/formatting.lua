d9bc81707729718aaf550d8228e3ac812aea68a3
Krishnakumar Gopalakrishnan <krishna.kumar@ucl.ac.uk>
2022-10-05 16:19:30 +0100

    minor edits to disabled formatters list in formatting.lua

--- lua/user/lsp/formatting.lua
+++ lua/user/lsp/formatting.lua
@@ -1,10 +1,10 @@
 return {
-  disabled = { "sumneko_lua" },
-  filter = function(client)
+  disabled = { -- disable formatting capabilities for the listed clients
+    -- "sumneko_lua",
+  },
+  filter = function(client) -- fully override the default formatting function
     -- only enable null-ls for javascript files
-    if vim.bo.filetype == "javascript" then
-      return client.name == "null-ls"
-    end
+    if vim.bo.filetype == "javascript" then return client.name == "null-ls" end
 
     -- enable all other clients
     return true
