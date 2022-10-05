4ed6f95c0e7ea08fa03b5b4ea0c8f305a5627046
Krishnakumar Gopalakrishnan <krishna.kumar@ucl.ac.uk>
2022-10-05 16:18:44 +0100

    diagnostic highlights deleted from highlights.lua file

--- lua/user/highlights.lua
+++ lua/user/highlights.lua
@@ -1,19 +1,6 @@
--- Override highlight groups in any theme
+-- Add highlight groups in any theme
 return {
   -- duskfox = { -- a table of overrides/changes to the default
   --   Normal = { bg = "#000000" },
   -- },
-  default_theme = function(highlights) -- or a function that returns a new table of colors to set
-    local C = require "default_theme.colors"
-
-    highlights.Normal = { fg = C.fg, bg = C.bg }
-
-    -- New approach instead of diagnostic_style
-    highlights.DiagnosticError.italic = true
-    highlights.DiagnosticHint.italic = true
-    highlights.DiagnosticInfo.italic = true
-    highlights.DiagnosticWarn.italic = true
-
-    return highlights
-  end,
 }
