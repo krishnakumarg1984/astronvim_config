4d4edecd1a4c0590eb04f5cb2c1be1ee1e115604
Krishnakumar Gopalakrishnan <krishna.kumar@ucl.ac.uk>
2022-10-05 16:21:05 +0100

    changes the servers.lua table to conditionally install certain LSPs depending on runtime executable availability of python

--- lua/user/lsp/servers.lua
+++ lua/user/lsp/servers.lua
@@ -1,5 +1,6 @@
+-- The names are based on the completion candidates for ':LspInfo'
 -- Extend LSP configuration
-return {
+servers = {
   -- "arduino_language_server", -- written in 'go'
   -- "asm_lsp", -- written in 'rust'
   -- "ccls",
@@ -17,3 +18,7 @@ return {
   -- "taplo", -- problematic with rust/cargo installs
   -- "zk", -- link not found (404 error)
 }
+
+if vim.fn.executable "python3" == 1 then table.insert(servers, "pylsp") end
+
+return servers
