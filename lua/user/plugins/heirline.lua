abbaae54b068149cfbe479c5d492ae76fc4a30b9
Krishnakumar Gopalakrishnan <krishna.kumar@ucl.ac.uk>
2022-10-31 14:55:35 +0000

    fixes a heirline config bug

--- lua/user/plugins/heirline.lua
+++ lua/user/plugins/heirline.lua
@@ -78,7 +78,7 @@ return function(config)
     -- add a component for the current diagnostics if it exists and use the right separator for the section
     astronvim.status.component.diagnostics { surround = { separator = "right" } },
     -- add a component to display LSP clients, disable showing LSP progress, and use the right separator
-    astronvim.status.component.lsp { lsp_progress = true, surround = { separator = "right" } },
+    astronvim.status.component.lsp { lsp_progress = false, surround = { separator = "right" } },
     -- NvChad has some nice icons to go along with information, so we can create a parent component to do this
     -- all of the children of this table will be treated together as a single component
     astronvim.status.component.treesitter {
