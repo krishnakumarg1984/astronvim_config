5a28d5f71913b11857d8b038f8de9a14d6713b0e
Krishnakumar Gopalakrishnan <krishna.kumar@ucl.ac.uk>
2022-12-07 23:28:08 +0000

    removes filetype from lsplines loading!

--- lua/user/plugins/lsplines_nvim/init.lua
+++ lua/user/plugins/lsplines_nvim/init.lua
@@ -1,18 +1,4 @@
 return {
-  ft = {
-    "c",
-    "cmake",
-    "cpp",
-    "cuda",
-    "lua",
-    "markdown",
-    "opencl",
-    "perl",
-    "python",
-    "rust",
-    "sh",
-    "yaml",
-  },
   -- module = "lsp_lines",
   wants = { "mason-lspconfig.nvim" }, -- make sure to load after mason-lspconfig
   config = function() require "user.plugins.lsplines_nvim.config" end,
