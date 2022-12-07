34e210ad23934088379fdede049b866e3a484392
Krishnakumar Gopalakrishnan <krishna.kumar@ucl.ac.uk>
2022-12-07 23:14:06 +0000

    simplified neomakelsp installation and configuration

--- lua/user/plugins/mason-lspconfig.lua
+++ lua/user/plugins/mason-lspconfig.lua
@@ -15,9 +15,6 @@ local my_ensure_installed = {
 }
 
 if vim.fn.executable "clang" == 1 then table.insert(my_ensure_installed, "clangd") end -- requires a reasonably new version of glibc
-if vim.fn.executable "neocmakelsp" ~= 1 and vim.fn.executable "cmake" == 1 then
-  table.insert(my_ensure_installed, "cmake")
-end
 -- if vim.fn.executable "julia" then table.insert(my_ensure_installed, "julials") end
 if vim.fn.executable "node" == 1 and vim.fn.executable "bash" == 1 then table.insert(my_ensure_installed, "bashls") end
 if vim.fn.executable "node" == 1 then table.insert(my_ensure_installed, "vimls") end
@@ -32,6 +29,14 @@ if vim.fn.executable "tsc" == 1 then table.insert(my_ensure_installed, "yamlls")
 if vim.fn.executable "tsc" and vim.fn.executable "perl" == 1 then table.insert(my_ensure_installed, "perlnavigator") end
 if vim.fn.executable "rustc" == 1 and vim.fn.executable "cargo" == 1 then
   table.insert(my_ensure_installed, "rust_analyzer")
+end
+
+if vim.fn.executable "cmake" == 1 then
+  if vim.fn.has "macunix" and vim.fn.executable "rustc" == 1 and vim.fn.executable "cargo" then
+    table.insert(my_ensure_installed, "neocmake")
+  else
+    table.insert(my_ensure_installed, "cmake")
+  end
 end
 
 return {
