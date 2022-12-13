ce7dfccf8cde45c567c0ce6f4ae92ba3196c674f
Krishnakumar Gopalakrishnan <krishna.kumar@ucl.ac.uk>
2022-12-13 15:35:12 +0000

    updates to cmake_for_vim config

--- lua/user/plugins/cmake_for_vim/config.lua
+++ lua/user/plugins/cmake_for_vim/config.lua
@@ -1,3 +1,9 @@
+-- https://neovim.discourse.group/t/how-to-get-number-of-processors-in-linux-with-lua-api/1943
+-- if vim.fn.has("win32") > 0 then
+--     u.num_of_processers = vim.env.NUMBER_OF_PROCESSORS
+-- elseif fn.has("unix") > 0 then
+--     u.num_of_processers = 4 -- os.execute("nproc") return wrong value
+-- end
 return {
   require "telescope",
   vim.keymap.set("n", "<leader>cb", "<Plug>(CMakeBuild)", { desc = "Build current project" }),
@@ -9,6 +15,20 @@ return {
   vim.keymap.set("n", "<leader>cm", "<cmd>CCMake tab<CR>", { desc = "Console cmake" }),
   vim.keymap.set("n", "<leader>cR", "<Plug>(CMakeReset)", { desc = "Remove build dir" }),
   vim.keymap.set("n", "<leader>cr", "<Plug>(CMakeRun)", { desc = "Run binary of current target" }),
-  vim.keymap.set("n", "<leader>cT", "<Plug>(CTest)", { desc = "CTest (with args)" }),
-  vim.keymap.set("n", "<leader>ct", "<Plug>(CTestCurrent)", { desc = "CTest (current target)" }),
+  vim.keymap.set("n", "<leader>ctt", "<Plug>(CTest)", { desc = "CTest" }),
+  vim.keymap.set("n", "<leader>ctc", "<Plug>(CTestCurrent)", { desc = "CTest (current target)" }),
+  vim.keymap.set("n", "<leader>cto", "<cmd>CTest --output-on-failure<CR>", { desc = "CTest (output on failure)" }),
+  vim.keymap.set("n", "<leader>ctv", "<cmd>CTest --verbose<CR>", { desc = "CTest (verbose)" }),
+  vim.keymap.set(
+    "n",
+    "<leader>ctV",
+    "<cmd>CTest --verbose --output-on-failure<CR>",
+    { desc = "CTest (verbose + output on failure)" }
+  ),
+  vim.keymap.set(
+    "n",
+    "<leader>ctr",
+    "<cmd>CTest --rerun-failed --output-on-failure<CR>",
+    { desc = "CTest (rerun failed)" }
+  ),
 }
