8984c49ea20f0b6138958848fdc37b187d016fad
Krishnakumar Gopalakrishnan <krishna.kumar@ucl.ac.uk>
2023-01-20 15:34:30 +0530

    updates to overseer_nvim plugin

--- lua/user/plugins/overseer_nvim/config.lua
+++ lua/user/plugins/overseer_nvim/config.lua
@@ -1,9 +1,11 @@
 require("overseer").setup {
+  -- strategy = "toggleterm",
   -- templates = { "builtin", "user.cpp_build" },
   component_aliases = {
     default = {
       -- These are the default components listed in the README
       -- You probably want to keep them
+      { "display_duration", detail_level = 2 },
       "on_output_summarize",
       "on_exit_set_status",
       "on_complete_notify",
@@ -16,7 +18,10 @@ require("overseer").setup {
     default_vscode = {
       "default",
       -- "on_output_quickfix",
-      { "on_output_quickfix", open = true },
+      "on_result_diagnostics",
+      { "on_output_quickfix", open_on_match = true },
+      -- { "on_output_quickfix", open = true },
     },
   },
 }
+vim.keymap.set("n", "<leader>or", "<cmd>OverseerRun<CR>", { desc = "OverseerRun" })
