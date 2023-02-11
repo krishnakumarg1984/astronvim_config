07d531329c6c8d4da3697fdf1e1bed2a47ead2e2
Krishnakumar Gopalakrishnan <krishna.kumar@ucl.ac.uk>
2023-02-11 14:30:47 +0530

    updates some heirline content

--- lua/user/heirline/colors.lua
+++ lua/user/heirline/colors.lua
@@ -1,14 +1,18 @@
 -- add new colors that can be used by heirline
 return {
-  blank_bg = "#5c6370",
-  file_info_bg = "#3e4452",
   -- background and foreground of modified file
-  file_info_modified_bg = "#EBEEEE",
+  -- blank_bg = "#5c6370",
+  -- buffer_bg = "#16161E",
+  -- file_info_bg = "#3e4452",
   -- file_info_modified_bg = "#5f27d8",
   -- file_info_modified_bg = "#efcfff",
-  file_info_modified_fg = "#1e222a",
-  nav_icon_bg = "#89b06d",
-  -- folder_icon_bg = "#ec5f67",
   -- folder_icon_bg = "#a86357",
-  folder_icon_bg = "#aaaaaa",
+  -- folder_icon_bg = "#ec5f67",
+  -- tabline_bg = "none",
+  -- blank_bg = "#545c7e",
+  -- file_info_bg = "#3b4261",
+  -- file_info_modified_bg = "#EBEEEE",
+  -- file_info_modified_fg = "#1e222a",
+  -- folder_icon_bg = "#aaaaaa",
+  -- nav_icon_bg = "#89b06d",
 }
--- lua/user/heirline/separators.lua
+++ lua/user/heirline/separators.lua
@@ -1,7 +1,13 @@
 -- Customize different separators between sections
 return {
+  -- left = { "", "  " }, -- separator for the left side of the statusline
+  -- space_left = { " ", "" }
+  -- space_right = { "", " " },
+  -- tab = { "", "" },
+  -- tab = { "", "" },
+  -- tab_left = { "", "" },
   breadcrumbs = " ",
   left = { "", " " }, -- separator for the left side of the statusline
   right = { " ", "" }, -- separator for the right side of the statusline
-  tab = { "", "" },
+  right_accent = { "", " " }, -- accent separator for the right side of the statusline
 }
