3bd0193feb27046f89441cc59b6c521120a44ab2
Krishnakumar Gopalakrishnan <krishna.kumar@ucl.ac.uk>
2023-05-09 18:09:01 +0100

    adds two gruvbox colorschemes

---
+++ lua/user/plugins/gruvbox_baby.lua
@@ -0,0 +1,11 @@
+return {
+  "luisiacc/gruvbox-baby", --  Gruvbox theme for neovim with full christmas_treeTreeSitter support.
+  lazy = false,
+  -- config = function()
+  --   require("kanagawa").setup {
+  --     compile = true,
+  --     dimInactive = false, -- dim inactive window `:h hl-NormalNC`
+  --     -- dimInactive = true, -- dim inactive window `:h hl-NormalNC`
+  --   }
+  -- end,
+}
