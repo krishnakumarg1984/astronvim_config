15ec0f78eeac6f40ecf2d9c2643dbdfe8d21dc12
Krishnakumar Gopalakrishnan <krishna.kumar@ucl.ac.uk>
2023-05-09 18:06:03 +0100

    stylua formatting of workspace_diagnostics keymap

--- lua/user/plugins/lsp_plugins.lua
+++ lua/user/plugins/lsp_plugins.lua
@@ -152,7 +152,12 @@ return {
       vim.keymap.set("n", "<leader>xd", "<cmd>Trouble document_diagnostics<cr>", { desc = "Show document diagnostics" })
       vim.keymap.set("n", "<leader>xl", "<cmd>Trouble loclist<cr>", { desc = "Diagnostics (loc list)" })
       vim.keymap.set("n", "<leader>xq", "<cmd>Trouble quickfix<cr>", { desc = "Diagnostics (quickfix list)" })
-      vim.keymap.set("n", "<leader>xw", "<cmd>Trouble workspace_diagnostics<cr>", { desc = "Show workspace diagnostics" })
+      vim.keymap.set(
+        "n",
+        "<leader>xw",
+        "<cmd>Trouble workspace_diagnostics<cr>",
+        { desc = "Show workspace diagnostics" }
+      )
       vim.keymap.set("n", "<leader>xx", "<cmd>TroubleToggle<cr>", { desc = "Toggle diagnostics window" })
       vim.keymap.set("n", "gR", "<cmd>Trouble lsp_references<cr>", { desc = "Symbol references (diagnostics window)" })
     end,
