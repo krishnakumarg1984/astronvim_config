15e4e867e936b281350b0547bc8b6ac259bb2ed3
Krishnakumar Gopalakrishnan <krishna.kumar@ucl.ac.uk>
2023-05-09 18:06:36 +0100

    loads rust.vim plugin on bufread cargo.toml event as well

--- lua/user/plugins/vimscript_plugins.lua
+++ lua/user/plugins/vimscript_plugins.lua
@@ -7,6 +7,7 @@ return {
   },
   {
     "rust-lang/rust.vim", -- Vim configuration for Rust. (must be loaded before 'rust-tools')
+    event = { "BufRead Cargo.toml" },
     ft = { "rust" },
   },
   {
