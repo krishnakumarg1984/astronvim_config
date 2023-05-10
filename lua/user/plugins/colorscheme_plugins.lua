e8b3d2b55f524a7a823a67ada6b58c47050bff28
Krishnakumar Gopalakrishnan <krishna.kumar@ucl.ac.uk>
2023-05-10 16:23:57 +0100

    consolidated all colorscheme config files into one

---
+++ lua/user/plugins/colorscheme_plugins.lua
@@ -0,0 +1,21 @@
+return {
+  {
+    "rebelot/kanagawa.nvim", -- Colorscheme inspired by the colors of the famous painting by Katsushika Hokusai
+    lazy = false,
+    config = function()
+      require("kanagawa").setup {
+        compile = true,
+        dimInactive = false, -- dim inactive window `:h hl-NormalNC`
+        -- dimInactive = true, -- dim inactive window `:h hl-NormalNC`
+      }
+    end,
+  },
+  -- {
+  --   "luisiacc/gruvbox-baby", --  Gruvbox theme for neovim with full christmas_treeTreeSitter support.
+  --   cmd = { "Telescope colorscheme" },
+  -- },
+  -- {
+  --   "sainnhe/gruvbox-material", --  Gruvbox with Material Palette
+  --   lazy = false,
+  -- },
+}
