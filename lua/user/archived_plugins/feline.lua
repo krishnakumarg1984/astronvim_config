e32cba9ef675db9a2fb58d2bda0b0c37cbac6ece
Krishnakumar Gopalakrishnan <krishna.kumar@ucl.ac.uk>
2023-07-11 17:29:30 +0100

    removes feline.nvim

--- lua/user/archived_plugins/feline.lua
+++
@@ -1,6 +0,0 @@
-return function(config)
-  -- config.components.active[2][6] = { provider = { name = "position", opts = { padding = true } } }
-  config.components.active[2][6] = { provider = { name = "position", opts = { padding = { line = 4, col = 2 } } } }
-  -- vim.pretty_print(config.components.active[2])
-  return config
-end
