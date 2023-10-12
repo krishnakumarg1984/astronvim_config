083cfd28ff5f32c7fa03e8896ac949493762ee9b
Krishnakumar Gopalakrishnan <krishna.kumar@ucl.ac.uk>
2023-10-12 13:37:08 +0100

    adds gitignore nvim

--- lua/user/plugins/utility.lua
+++ lua/user/plugins/utility.lua
@@ -1,5 +1,9 @@
 return {
   {
+    "wintermute-cell/gitignore.nvim", -- A neovim plugin for generating .gitignore files.
+    cmd = { "Gitignore" },
+  },
+  {
     "jghauser/mkdir.nvim", -- This neovim plugin creates missing folders on save
     event = "BufWritePre",
   },
