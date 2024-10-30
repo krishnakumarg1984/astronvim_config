5fdddded76f53014438ac139f84e28d94b3066c8
Krishnakumar Gopalakrishnan <krishna.kumar@ucl.ac.uk>
2024-10-30 11:19:56 +0000

    cleans up base.lua

--- lua/base.lua
+++ lua/base.lua
@@ -7,9 +7,8 @@ return {
     opts = { -- AstroNvim options must be set here with the `import` key
       mapleader = "\\", -- This ensures the leader key must be configured before Lazy is set up
       maplocalleader = "\\", -- This ensures the localleader key must be configured before Lazy is set up
-      icons_enabled = true, -- Set to false to disable icons (if no Nerd Font is available)
-      pin_plugins = nil, -- Default will pin plugins when tracking `version` of AstroNvim, set to true/false to override
-      update_notifications = true, -- Enable/disable notification about running `:Lazy update` twice to update pinned plugins
+      -- icons_enabled = true, -- Default is true. Set false to disable icons (if nerd font isn't available)
+      pin_plugins = nil, -- boolean. whether to pin plugins or not, if `nil` then will pin if version is set.
     },
   },
   -- { "AstroNvim/astrocommunity" },
